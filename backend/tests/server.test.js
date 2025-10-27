11e5ecb807a3091ee493b79b820ee6f09f74b617
oggy3682 <tariyonlannister3682@gmail.com>
2025-10-27 23:17:20 +0530

    fixed the test.js file

--- backend/tests/server.test.js
+++ backend/tests/server.test.js
@@ -1,14 +1,19 @@
 const request = require('supertest');
 const app = require('../server');
-const server = app.listen(); // temporary instance for cleanup
+
+let server;
+
+beforeAll(() => {
+  server = app.listen(5001);
+});
+
+afterAll((done) => {
+  server.close(done);
+});
 
 describe('API tests', () => {
-  afterAll(() => {
-    server.close(); // stop Express after tests
-  });
-
   it('should return hello message', async () => {
-    const res = await request(app).get('/api/message');
+    const res = await request(server).get('/api/message');
     expect(res.statusCode).toBe(200);
     expect(res.body.message).toBe('Hello from the backend! 🚀 oggy');
   });
